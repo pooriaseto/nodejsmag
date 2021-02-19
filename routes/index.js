e87956718a8a8e8c4875e33e66c008927b756004
Pooria Home <pooriaseto@gmail.com>
2021-02-19 11:28:07 +0330

    404 Notfound page added

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/about-us', function (req, res, next) {
   res.render('aboutUs', { title: 'AbousUs' });
 });
 
+router.get('/notfound', function (req, res, next) {
+  res.render('notFound', { title: 'notfound' });
+});
+
 module.exports = router;
