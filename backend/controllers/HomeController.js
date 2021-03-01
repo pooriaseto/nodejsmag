904b0f2623a92b1b579420bf4a87197fb090cc6b
o.setayesh <pooriaseto@gmail.com>
2021-03-01 21:41:04 +0330

    add query to get post categories for home page

--- backend/controllers/HomeController.js
+++ backend/controllers/HomeController.js
@@ -1,8 +1,12 @@
-const {Post,Category} = require("../models/db");
+const { Post, Category } = require("../models/db");
 
 class HomeController {
   async index(req, res) {
-    let posts = await Post.findAll();
+    let posts = await Post.findAll({
+      include: { model: Category, as: "categories" },
+    });
+
+    
     let subjects = await Category.findAll();
 
     res.status(200).render("index", { title: "index", posts, subjects });
