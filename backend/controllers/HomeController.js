3407b2f82481918088f7b3841da65f8b72c39f22
o.setayesh <pooriaseto@gmail.com>
2021-03-01 22:36:52 +0330

    add findAndCount to home page

--- backend/controllers/HomeController.js
+++ backend/controllers/HomeController.js
@@ -2,17 +2,17 @@ const { Post, Category } = require("../models/db");
 
 class HomeController {
   async index(req, res) {
-    let posts = await Post.findAll({
-      include: { model: Category, as: "categories" },
+    let query = await Post.findAndCountAll({
+      include: { model: Category, as: "categories", required: true },
       order: [["creation_time", "DESC"]],
       limit: 1,
       offset: 0,
     });
 
-    
+
     let subjects = await Category.findAll();
 
-    res.status(200).render("index", { title: "index", posts, subjects });
+    res.status(200).render("index", { title: "index", posts: query.rows, subjects });
   }
 }
 
