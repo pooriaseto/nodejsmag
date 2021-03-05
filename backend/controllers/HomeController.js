e7c611a417142dbcc9c6095114bd746c6cb51767
o.setayesh <pooriaseto@gmail.com>
2021-03-05 20:48:53 +0330

    add search route to project

--- backend/controllers/HomeController.js
+++ backend/controllers/HomeController.js
@@ -1,8 +1,51 @@
+const { Op } = require("sequelize");
 const { Post, Category } = require("../models/db");
 const { PostsPageSize, mainPath } = require("../config/public");
 const DateTime = require("../utils/dateTime");
 
 class HomeController {
+  async search(req, res, next) {
+    let q = req.query.q;
+    res.locals.app.q = q;
+    if (!q) {
+      res.redirect(301, mainPath);
+    }
+
+    let posts = await Post.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: "%" + q + "%",
+            },
+          },
+          {
+            description: {
+              [Op.like]: "%" + q + "%",
+            },
+          },
+        ],
+      },
+      distinct: true,
+      attributes: ["title", "slug", "imageUrl", "creation_time"],
+      order: [["creation_time", "DESC"]],
+      limit: PostsPageSize,
+      offset: 0,
+      include: {
+        model: Category,
+        as: "categories",
+        attributes: ["title", "slug", "parentId", "id"],
+        required: true,
+      },
+    });
+
+    res.status(200).render("search", {
+      title: "جستحوی " + q,
+      posts,
+      convertToPersianDate: DateTime.convertToPersianDate,
+    });
+  }
+
   async index(req, res, next) {
     let query = await Post.findAndCountAll({
       distinct: true,
@@ -19,7 +62,7 @@ class HomeController {
     });
 
     let subjects = await Category.findAll({
-      where : {parentId : 0},
+      where: { parentId: 0 },
       attributes: ["title", "slug", "imageUrl"],
       limit: 6,
       offset: 0,
@@ -51,7 +94,6 @@ class HomeController {
         },
       ],
     });
-
 
     let chosenPosts = true;
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const homeController = require("../controllers/HomeController");
 const BlogController = require("../controllers/BlogController");
+
+router.get("/search", homeController.search);
 
 router.get("/page/:pageNumber", homeController.indexPagination);
 router.get("/", homeController.index);
@@ -19,15 +21,17 @@ router.get(
   "/:mainCategorySlug/:subCategorySlug/page/:pageNumber",
   BlogController.subCategoryPagination
 );
+
 router.get(
   "/:mainCategorySlug/page/:pageNumber",
   BlogController.mainCategoryPagination
 );
+
 router.get("/:mainCategory/:subCategory/:slug", BlogController.singlePost);
+
 router.get("/:mainCategorySlug/:subCategorySlug", BlogController.subCategory);
+
 router.get("/:mainCategorySlug", BlogController.mainCategory);
-
-
 
 router.all("*", (req, res, next) => {
   res.render("notFound", { title: "متاسفانه صفحه مورد نظر یافت نشد." });
